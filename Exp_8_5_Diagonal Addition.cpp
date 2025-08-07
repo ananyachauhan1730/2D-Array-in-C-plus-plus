3b0858323ef9dee2b12cce622803baa0a4fd16c1
ananyachauhan1730 <chauhanananya603@gmail.com>
2025-08-07 16:11:17 +0530

    Update Exp_8_5_Diagonal Addition.cpp

--- Exp_8_5_Diagonal Addition.cpp
+++ Exp_8_5_Diagonal Addition.cpp
@@ -1,1 +1,19 @@
+include <iostream>
+using namespace std;
 
+int main() {
+    int A[10][10], n, sum = 0;
+    cout << "Enter size of square matrix (n x n): ";
+    cin >> n;
+
+    cout << "Enter matrix A:\n";
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            cin >> A[i][j];
+
+    for(int i = 0; i < n; i++)
+        sum += A[i][i];
+
+    cout << "Diagonal Sum: " << sum << endl;
+    return 0;
+}
